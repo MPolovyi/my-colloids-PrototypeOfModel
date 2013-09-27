b0278e9d452e5895415d2f28bb846f0049de31bf
Maximus <Mr_Destiny@list.ru>
2013-09-27 14:29:06 +0300

    minor changes

--- PrototypeModel/Lattice.cs
+++ PrototypeModel/Lattice.cs
@@ -11,14 +11,14 @@ namespace PrototypeModel
         private Point[] _directions =
             {
                 new Point(0, 0),
-                new Point(1, 0),
-                new Point(0, -1),
                 new Point(-1, 0),
                 new Point(0, 1),
-                new Point(1, -1),
-                new Point(-1, -1),
+                new Point(1, 0),
+                new Point(0, -1),
                 new Point(-1, 1),
-                new Point(1, 1)
+                new Point(1, 1),
+                new Point(1, -1),
+                new Point(-1, -1)
             };
 
 
--- PrototypeModel/World.cs
+++ PrototypeModel/World.cs
@@ -14,7 +14,7 @@ namespace PrototypeModel
 
         private string[] _map =
             {
-                "+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>+",
+                "++++++++++++++++++++++++++++++++++++++++",
                 ">                                      >",
                 ">                                      >",
                 ">                                      >",
@@ -53,20 +53,20 @@ namespace PrototypeModel
                 ">                                      >",
                 ">                                      >",
                 ">                                      >",
-                "+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>+"
+                "++++++++++++++++++++++++++++++++++++++++"
             };
 
         private Point[] _directions =
             {
                 new Point(0, 0),
-                new Point(1, 0),
-                new Point(0, -1),
                 new Point(-1, 0),
                 new Point(0, 1),
-                new Point(1, -1),
-                new Point(-1, -1),
+                new Point(1, 0),
+                new Point(0, -1),
                 new Point(-1, 1),
-                new Point(1, 1)
+                new Point(1, 1),
+                new Point(1, -1),
+                new Point(-1, -1)
             };
         
         private Bitmap WorldMap;
